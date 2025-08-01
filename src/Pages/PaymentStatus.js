1127b1d053a819c3033d24e2e3d6da41f5d07eb5
ramandeep <ramandeep@pixlerlab.com>
2025-08-01 16:41:46 +0530

    text color changed

--- src/Pages/PaymentStatus.js
+++ src/Pages/PaymentStatus.js
@@ -27,7 +27,7 @@ const PaymentStatus = () => {
     return (
       <div className="payment-model">
             <div className="payment-model-inner">
-                <h2 className="text-xl font-bold mb-4 text-center" style={{color:"red"}}>Payment Status</h2>
+                <h2 className="text-xl font-bold mb-4 text-center">Payment Status</h2>
                 <div className="text-center">
                     <h5 className="text-md font-semibold text-gray-700 mb-2">Message :</h5>
                     <div className="flex justify-center">
