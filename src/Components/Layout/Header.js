e86c4cd5b0874512b3ce33a90a15aad5ac06ccd3
Nitesh <nitesh@pixlerlab.com>
2025-08-08 12:26:07 +0530

    changes

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -54,29 +54,29 @@ export default function Header() {
   const hideSearchbarCollapse =
   location.pathname === "/requests" || role === Roles.SERVICE_PROVIDER;
 
-  const getProfileApiCall = async () => {
-    if (role === "3") {
-      expiredSubscriptionPopup();
-    } else {
-      let apiRes = await dispatch(CustomerActions.getProfile());
-      if (apiRes?.payload?.success) {
-      if(apiRes.payload?.data.email_verified==0){
-        Navigate(`/otp-varification?userId=${apiRes.payload?.data?._id}`, { replace: true });
-      }else if(apiRes.payload?.data?.is_completeProfile == 0){
-        Navigate("/complete-profile", { replace: true });
-        toast.success("Please Complete Your Profile.");
-      }
-        localStorage.setItem('ServiceLimit',  JSON.stringify(apiRes?.payload?.data))
-        dispatch(setCustomer(apiRes?.payload?.data));
-      }
-    }
-  };
+  // const getProfileApiCall = async () => {
+  //   if (role === "3") {
+  //     expiredSubscriptionPopup();
+  //   } else {
+  //     let apiRes = await dispatch(CustomerActions.getProfile());
+  //     if (apiRes?.payload?.success) {
+  //     if(apiRes.payload?.data.email_verified==0){
+  //       Navigate(`/otp-varification?userId=${apiRes.payload?.data?._id}`, { replace: true });
+  //     }else if(apiRes.payload?.data?.is_completeProfile == 0){
+  //       Navigate("/complete-profile", { replace: true });
+  //       toast.success("Please Complete Your Profile.");
+  //     }
+  //       localStorage.setItem('ServiceLimit',  JSON.stringify(apiRes?.payload?.data))
+  //       dispatch(setCustomer(apiRes?.payload?.data));
+  //     }
+  //   }
+  // };
 
-  useEffect(() => {
-    if (token) {
-      getProfileApiCall();
-    }
-  }, [token]);
+  // useEffect(() => {
+  //   if (token) {
+  //     getProfileApiCall();
+  //   }
+  // }, [token]);
 
   useEffect(() => {
     dispatch(CustomerActions.notificationListing());
