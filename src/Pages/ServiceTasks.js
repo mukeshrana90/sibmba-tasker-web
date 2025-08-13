db326e38417608b9b97f0a5e6929ae1cff61c392
ramandeep <ramandeep@pixlerlab.com>
2025-08-13 12:42:16 +0530

    default Image added

--- src/Pages/ServiceTasks.js
+++ src/Pages/ServiceTasks.js
@@ -211,7 +211,7 @@ export default function ServiceTasks() {
           src={
             task?.images?.length > 0
               ? `${process.env.REACT_APP_API_URLL}/${task.images[0]}`
-              : ""
+              : defaultImage
           }
           alt="Task"
           className="task-image"
